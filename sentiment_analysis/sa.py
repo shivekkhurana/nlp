61fd62a0bd85c8494ab9e3ebcda7af6d626af69f
Shivek Khurana <khuranashivek@gmail.com>
2012-11-05 06:22:13 +0530

    training based on corpus and counting words in sentence complete

--- sentiment_analysis/sa.py
+++ sentiment_analysis/sa.py
@@ -2,19 +2,20 @@
 
 import collections
 
-def n_gram_data(file):
+def train(corpus):
   '''
-  Populate n-gram data into memory.
+  Populate training data into memory.
 
   @param:
-    name of file containg n-gram data
+    name of file containg n-gram data(+ tweets)
   @return:
-    dict() with key as n-gram and value as a list.
-    ex: {"spoiled" : [-1,...], "unpredicatable movie":[1,...]}
-      first element of list is polarity : maybe -1, 1, 0
+    list() of tuples
+    ex: [("spoiled",-1,...), ("unpredicatable movie",1,...)]
+      first element of tuple is n-gram entity
+      next is polarity : maybe -1, 1, 0
   '''
-  f = open(file)
-  n_gram = {}
+  f = open(corpus)
+  n_gram = []
   #iterate over file to populate n_grams
   while True:
     line = f.readline()
@@ -22,12 +23,35 @@ def n_gram_data(file):
     if line[0] != '#' and line != '\n':
       #line is a list now
       line = line.strip().split(',')
-      n_gram[line[0]] = line[1]
+      n_gram.append( (line[0], line[1]) )
+  return n_gram
 
-def sentence(raw):
+def sentence_gram(raw_sentence):
   '''
-    
+  N-gram(ize) a sentence and try matching with corpus.
+
+  @param:
+    raw sentence
+  @return:
+    collections.Counter object, containg occurence of 
+    >3 letter word in raw_sentence. This insures that 
+    and, is, to, etc. are ignored.
   '''
+  cnt = collections.Counter()
+  w_list = raw_sentence.split(' ')
+  for word in w_list:
+    if len(word)>3:
+      #1 gram
+      cnt[word] += 1
+
+    #2 gram
+    try:
+      cnt[word + ' ' + w_list[ w_list.index(word) + 1] ] += 1
+    except IndexError, e:
+      pass
+
+  return cnt
 
 if __name__ == "__main__":
-  n_gram_data("n_gram.data")
+  print train("n_gram.txt")
+  print sentence_gram("to infinity and beyond")
